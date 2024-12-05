94495eee67a194ea186e993b9f9ec1a49d6688f5
Vikash Shukla <100182939+vshukla-code@users.noreply.github.com>
2024-12-05 17:51:00 +0530

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        document.getElementById('preloader').style.display = 'none';
+    }, 5000); // 30000ms = 30 seconds
+});
